6ac41fbc2d35a0955b2d02f7abda978e45288ad4
rewang <wangat2001@qq.com>
2017-10-13 16:57:47 +0800

    #3 init project (#4)
    * #3 init project
    
    * update gitignore, fix a bug when compile
    
    * remove idea/vcs.xml

---
+++ http-server/src/main/java/com/s2u2m/robot/http/HttpServer.java
@@ -0,0 +1,16 @@
+package com.s2u2m.robot.http;
+
+import java.net.InetSocketAddress;
+
+import com.s2u2m.net.ISvr;
+
+public class HttpServer implements ISvr {
+
+	public void startSvr(InetSocketAddress socketAddress) throws InterruptedException {
+
+	}
+
+	public void stopSvr() throws InterruptedException {
+
+	}
+}
---
+++ http-server/src/main/java/com/s2u2m/robot/http/MainLauncher.java
@@ -0,0 +1,8 @@
+package com.s2u2m.robot.http;
+
+public class MainLauncher {
+	public static void main(String[] args) {
+		String ss = "去s額image有没有";
+		System.out.print(ss.substring(0, ss.length() - 1));
+	}
+}
